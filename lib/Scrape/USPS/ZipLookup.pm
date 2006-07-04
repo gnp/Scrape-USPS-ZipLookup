27fae22fc2509ca286933e6e01b889a7a07925a2
Gregor N. Purdy, Sr <gnp@acm.org>
2006-07-04 17:10:33 +0000

    Updated TERMS OF USE

--- lib/Scrape/USPS/ZipLookup.pm
+++ lib/Scrape/USPS/ZipLookup.pm
@@ -389,11 +389,35 @@ To see debugging output, call C<< $zlu->verbose(1) >>.
 
 =head1 TERMS OF USE
 
-BE SURE TO READ AND FOLLOW THE UNITED STATES POSTAL SERVICE TERMS OF USE
-PAGE (AT C<http://www.usps.com/homearea/docs/termsofuse.htm> AT THE TIME
-THIS TEXT WAS WRITTEN). IN PARTICULAR, NOTE THAT THEY
-DO NOT PERMIT THE USE OF THEIR WEB SITE'S FUNCTIONALITY FOR COMMERCIAL
-PURPOSES. DO NOT USE THIS CODE IN A WAY THAT VIOLATES THE TERMS OF USE.
+BE SURE TO READ AND FOLLOW THE UNITED STATES POSTAL SERVICE TERMS OF USE PAGE
+(AT C<http://www.usps.com/homearea/docs/termsofuse.htm> AT THE TIME THIS TEXT
+WAS WRITTEN). IN PARTICULAR, NOTE THAT THEY DO NOT PERMIT THE USE OF THEIR WEB
+SITE'S FUNCTIONALITY FOR COMMERCIAL PURPOSES. DO NOT USE THIS CODE IN A WAY
+THAT VIOLATES THE TERMS OF USE.
+
+As the user of this code, you are responsible for complying with the most
+recent version of the Terms of Use, whether at the URL provided above or
+elsewhere if the U.S. Postal Service moves it or updates it. As a convenience,
+here is a copy of the most relevant paragraph of the Terms of Use as of
+2006-07-04:
+
+  Material on this site is the copyrighted property of the United States
+  Postal Service¨ (Postal Serviceª). All rights reserved. The information
+  and images presented here may not under any circumstances be reproduced
+  or used without prior written permission. Users may view and download
+  material from this site only for the following purposes: (a) for personal,
+  non-commercial home use; (b) where the materials clearly state that these
+  materials may be copied and reproduced according to the terms stated in
+  those particular pages; or (c) with the express written permission of the
+  Postal Service. In all other cases, you will need written permission from
+  the Postal Service to reproduce, republish, upload, post, transmit,
+  distribute or publicly display material from this Web site. Users agree not
+  to use the site for sale, trade or other commercial purposes. Users may not
+  use language that is threatening, abusive, vulgar, discourteous or criminal.
+  Users also may not post or transmit information or materials that would
+  violate rights of any third party or which contains a virus or other harmful
+  component. The Postal Service reserves the right to remove or edit any
+  messages or material submitted by users. 
 
 The author believes that the example usage given above does not violate
 these terms, but sole responsibility for conforming to the terms of use
@@ -417,4 +441,3 @@ This program is free software. It is subject to the same license as Perl.
 #
 # End of file.
 #
-
